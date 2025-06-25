8525fb0a6f8d20c58f68c1d0ba9fcb9ab5f5269c
Chad Collard <chadcollard1567@gmail.com>
2025-06-25 19:25:23 -0400

    need help lab 4-1

---
+++ lab4-1.py
@@ -0,0 +1,18 @@
+#Chad Collard
+#Lab 4-1
+#6/25/2025
+
+input("enter the temp ")
+input("fahrenheit or celsius ")
+    
+
+celsius = float(input("enter the temp in celsius: "))
+fahrenheit = (celsius * 9/5) + 32 
+print(fahrenheit)
+
+
+fahrenheit = float(input("enter the temp in fahrenheit: "))
+celsius = (fahrenheit -32) * 5/9
+print(celsius)
+
+
