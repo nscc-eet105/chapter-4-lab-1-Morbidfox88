2c1bf1e11ded92cb7f5ae6f9a8d248abf09c89e3
Chad Collard <chadcollard1567@gmail.com>
2025-07-18 22:12:11 -0400

    chapter 4-1 amend

--- lab4-1.py
+++ lab4-1.py
@@ -4,25 +4,28 @@
 
 def ctof(celsius):
     fahrenheit = (celsius * 9/5) + 32 
-    print(f'{fahrenheit:.2f}')
+    print(f"The current temperature is {celsius:.2f} celsius this temperature in fahrenheit is {fahrenheit:.2f} ")
 
 def ftoc(fahrenheit):
     celsius = (fahrenheit -32) * 5/9
     return(celsius)
 
 
-temp = float(input("enter the temp "))
-type_of_temp = input("fahrenheit or celsius ")
+temp = float(input("What is the current temperature? "))
 
+while True:
+    type_of_temp = input("Did you enter the temperature in (c)elsius of (f)ahrenheit? ")
+    if type_of_temp == "c":
+        ctof(temp)
+        break
 
-if type_of_temp == "c":
-    ctof(temp)
-
-elif type_of_temp == "f":
-    celsius = ftoc(temp)
-    print(f'{celsius:.2f}')
-
-
+    elif type_of_temp == "f":
+        celsius = ftoc(temp)
+        print(f"The current temperature is {temp:.2f} fahrenheit this temperature in celsius is {celsius:.2f} ")
+        break
+    else:
+        print('invalid input, please try again!')
+        continue
 
 
 
